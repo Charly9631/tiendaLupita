257390bfa5bf57a06395bff732e78805da1c73e8
juan <herreracharly99@gmail.com>
2023-02-03 20:07:07 -0600

    Implementacion del metodo para guardar un producto

--- src/main/java/com/app/tienda/controller/ProductoController.java
+++ src/main/java/com/app/tienda/controller/ProductoController.java
@@ -1,15 +1,43 @@
 package com.app.tienda.controller;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.app.tienda.model.Producto;
+import com.app.tienda.model.Usuario;
+import com.app.tienda.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+	private final Logger log = LoggerFactory.getLogger(ProductoController.class); 
+	
+	@Autowired
+	private ProductoService productoService;
+	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
 	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return"/productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		
+		log.info("este es un objeto {}",producto);
+		Usuario usuario = new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(usuario);
+		productoService.save(producto);
+		return "redirect:/productos";
+	}
+	
 }
